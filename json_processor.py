77cd9517d2a41cc8fc4bdc059bbadc4d3a507395
Joanne Lin <joannelin@Joannes-MacBook-Air.local>
2023-08-15 14:19:35 -0500

    json annotations cleaning

--- json_processor.py
+++ json_processor.py
@@ -1,6 +1,7 @@
 from path import my_path
 import pandas as pd
 import os
+import ast
 
 def attributes_json():
     
@@ -8,9 +9,9 @@ def attributes_json():
 
     
     columns = df.columns
-    columns.to_series().to_csv(output_csv_path, index=False)
+    columns.to_series().to_csv(output_csv_path_columns, index=False)
 
-    csv_dir = os.path.join(my_path, 'json_data', 'sag_T1_json_columns')
+    csv_dir = os.path.join(my_path, 'json_data', 'sag_IR_json_columns')
     os.makedirs(csv_dir, exist_ok=True)
 
     # Iterate through each column
@@ -28,9 +29,56 @@ def attributes_json():
         # Write the column data to the CSV file
         column_df.to_csv(column_csv_path, index=False)
 
+def annotations():
+    df = pd.read_json(file_path)
+    csv_dir = os.path.join(my_path, 'json_data', 'annotations_columns')
+    annotations_column = df['annotations']
+    print(annotations_column.head())
+    
+def id_result_box_raw():
+    df = pd.read_json(file_path)
+    data_df = pd.DataFrame(columns=['file_upload', 'results'])
+    for index, row in df.iterrows():
+        data_df = data_df.append({
+            'file_upload': row['file_upload'],
+            'results': row['annotations'][0]['result'], 
+            
+        }, ignore_index=True)
+    data_df.to_csv(output_csv_path_annotations_results, index=False)
+
+def id_result_clean():
+    df = pd.read_csv(results_raw)
+    clean_df = pd.DataFrame(columns=['image', 'original_width', 'original_height', 'image_rotation', 'value_x', 'value_y', 'value_width', 'value_height', 'value_rotation', 'value_rectanglelabels'])
+    for index, row in df.iterrows():
+        if len(ast.literal_eval(row['results']))>0:
+            for box in range(len(ast.literal_eval(row['results']))):
+                clean_df = clean_df.append({
+                    'image': row['image'],
+                    'original_width': ast.literal_eval(row['results'])[box]['original_width'], 
+                    'original_height': ast.literal_eval(row['results'])[box]['original_height'], 
+                    'image_rotation': ast.literal_eval(row['results'])[box]['image_rotation'], 
+                    
+                    'value_x': ast.literal_eval(row['results'])[box]['value']['x'], 
+                    'value_y': ast.literal_eval(row['results'])[box]['value']['y'], 
+                    'value_width': ast.literal_eval(row['results'])[box]['value']['width'], 
+                    'value_height': ast.literal_eval(row['results'])[box]['value']['height'], 
+                    'value_rotation': ast.literal_eval(row['results'])[box]['value']['rotation'], 
+                    'value_rectanglelabels': ast.literal_eval(row['results'])[box]['value']['rectanglelabels'], 
+                    
+                }, ignore_index=True)
+    clean_df.to_csv(output_csv_path_annotations_results, index=False)
+
 def main():
-    attributes_json()
+    # attributes_json()
+    # annotations()
+    # id_result_box_raw()
+    id_result_clean()
 
 if __name__ == '__main__':
-    file_path = os.path.join(my_path, 'Sag T1 Ankle Ricky 1-115.json')
-    output_csv_path = os.path.join(my_path, 'json_data', 'sag_T1_json_columns.csv')
+    file_path = os.path.join(my_path, 'SagIRAnkle2.0.json')
+
+    output_csv_path_columns = os.path.join(my_path, 'json_data', 'sag_IR_json_columns.csv')
+    output_csv_path_annotations_results = os.path.join(my_path, 'json_data', 'sag_IR_json_columns', 'annotations', 'results.csv')
+    results_raw = os.path.join(my_path, 'json_data', 'sag_IR_json_columns', 'annotations', 'results_raw(image_name_aligned).csv')
+    annotations_data_path = os.path.join(my_path, 'json_data', 'sag_IR_json_columns', 'annotations.csv')
+    main()
