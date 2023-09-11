53c0ed0249b3ef59c8df4a4a00cb9b7c4971cb0d
Joanne Lin <joannelin@lpc-216-74.lpc-wireless.depaul.edu>
2023-09-11 18:49:08 -0500

    by percent

--- bonding_box.py
+++ bonding_box.py
@@ -16,18 +16,19 @@ def crop_the_box():
                     
                     patient_id = patient[0:4]
 
-                    x = df.loc[df['image'] == filename, 'value_x'].values[0]
-                    y = (df.loc[df['image'] == filename, 'original_height'].values[0]) - (df.loc[df['image'] == filename, 'value_y'].values[0])
-                    width = df.loc[df['image'] == filename, 'value_width'].values[0]
-                    height = df.loc[df['image'] == filename, 'value_height'].values[0]
+                    x = df.loc[df['image'] == filename, 'x_by_percent'].values[0]
+                    # (df.loc[df['image'] == filename, 'original_height'].values[0]) - 
+                    y = (df.loc[df['image'] == filename, 'y_by_percent'].values[0])
+                    width = df.loc[df['image'] == filename, 'value_width_percent'].values[0]
+                    height = df.loc[df['image'] == filename, 'value_height_percent'].values[0]
 
                     image = Image.open(image_path)
 
-                    cropped_image = image.crop((x, y - height, x + width, y))
+                    cropped_image = image.crop((x, y , x + width, y + height))
 
 
                     #save the cropped image
-                    cropped_file_path = os.path.join(my_path, 'bonding_box', patient_id, filename[0:14])
+                    cropped_file_path = os.path.join(my_path, 'bonding_boxes_percent', patient_id, filename[0:14])
                     counter = 1
                     while os.path.exists(cropped_file_path + f"_{counter:02}.jpg"):
                         counter += 1
@@ -39,16 +40,16 @@ def bonding_box_folder():
     df = pd.read_csv(csv_path)
     df_patients = df['image'].apply(lambda x: x[0:4]).unique().tolist()
     for patient in df_patients:
-        folder_path = os.path.join(my_path, 'bonding_box', patient)
+        folder_path = os.path.join(my_path, 'bonding_boxes_percent', patient)
         os.makedirs(folder_path, exist_ok=True)
 
 def main():
-    bonding_box_folder()
+    # bonding_box_folder()
     crop_the_box()
     
 
 if __name__ == '__main__':
-    csv_path = os.path.join(my_path, 'json_data', 'sag_IR_json_columns', 'annotations', 'results_box_cleaned.csv')
+    csv_path = os.path.join(my_path, 'json_data', 'sag_IR_json_columns', 'annotations', 'results_box_cleaned_calculation.csv')
     folder_path = os.path.join(my_path, '06192023 SFI renamed')
     image_path = os.path.join(folder_path, 'P001 SAGIR', 'MRI ANKLE (LEFT) W_O CONT_5891215', 'P001 SAGIR_010.jpg')
 
